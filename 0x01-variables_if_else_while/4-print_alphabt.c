8e64ef737f15f2ba92cbc66754a1a3c8c7e78a17
Okolie James <zebedee788@gmail.com>
2022-09-09 13:42:23 -0700

    prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,17 @@
  */
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		putchar(low);
+		if (low != e && low != q)
+			putchar(low);
 	}
-	for (low = 'A'; low <= 'Z'; low++)
-	{
-		putchar(low);
-	}
-
-	putchar('\n');
+		printf("\n");
 
 	return (0);
 }
