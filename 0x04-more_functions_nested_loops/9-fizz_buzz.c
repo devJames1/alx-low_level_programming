0c7a2400802d50eda33d4d7bbe3bcf3aa1eb05d6
Okolie James <zebedee788@gmail.com>
2022-09-15 14:26:10 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,31 +5,21 @@
 *Return: Always 0 (Success)
 */
 
-int main(void)
+	int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
-{
+	{
 		if (i % 15 == 0)
-		{
 			printf("FizzBuzz ");
-
-		} else if ((i % 3) == 0)
-		{
+		else if (i % 3 == 0)
 			printf("Fizz ");
-
-		} else if ((i % 5) == 0)
-		{
+		else if (i % 5 == 0)
 			printf("Buzz ");
-
-		} else
-		{
+		else
 			printf("%d ", i);
-
-		}
-
-}
+	}
 	printf("\n");
 
 	return (0);
