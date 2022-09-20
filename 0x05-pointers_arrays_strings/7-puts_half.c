5db9899858b3076d218b6d60ce141bd1876b3eaf
Okolie James <zebedee788@gmail.com>
2022-09-20 12:03:46 -0700

    prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,52 @@
+include "holberton.h"
+
+/**
+ * puts_half - function prints half of a string
+ *
+ *@str: char pointer to string
+ *
+ * Return: 0 (success)
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int y;
+
+	if (len % 2 == 0)
+	{
+		y = len / 2;
+	}
+	else if (len % 2 != 0)
+	{
+		y = (len + 1) / 2;
+	}
+	for (; *(str + y) != '\0'; y++)
+	{
+		_putchar(*(str + y));
+	}
+	_putchar('\n');
+}
+
+
+/**
+ *_strlen - returns the length of a string.
+ *
+ *@s: pointer to char
+ *
+ *Return: 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	char *_str = s;
+	int cnt = 0;
+
+	while (*_str != '\0')
+	{
+		cnt++;
+		_str++;
+	}
+
+	return (cnt);
+}
