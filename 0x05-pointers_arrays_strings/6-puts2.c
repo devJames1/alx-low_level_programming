ad970f94a1a69f6980cffc876b6375e9aff3974c
Okolie James <zebedee788@gmail.com>
2022-09-20 11:57:26 -0700

    prints one char out of 2 of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *puts2 - function prints one char out of 2 of a string
+ *
+ *@str: pointer to char
+ *
+ *Return: 0 (success)
+ */
+
+ void puts2(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(*str);
+			str++;
+			count++;
+		}
+		else
+		{
+			str++;
+			count++;
+		}
+	}
+	_putchar('\n');
+}
