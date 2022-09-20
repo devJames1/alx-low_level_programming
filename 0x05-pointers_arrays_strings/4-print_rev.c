80f4fe94869676a21c9e6422194e033b52f01699
Okolie James <zebedee788@gmail.com>
2022-09-20 11:26:08 -0700

    prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,7 @@
  *Retutn: 0 (success)
  */
 
+
 void print_rev(char *s)
 {
 	int l = _strlen(s);
@@ -20,4 +21,26 @@ void print_rev(char *s)
 	}
 
 	_putchar('\n');
+}
+
+/**
+ *_strlen - returns the length of a string.
+ *
+ *@s: pointer to char
+ *
+ *Return: 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	char *_str = s;
+	int cnt = 0;
+
+	while (*_str != '\0')
+	{
+		cnt++;
+		_str++;
+	}
+
+	return (cnt);
 }
