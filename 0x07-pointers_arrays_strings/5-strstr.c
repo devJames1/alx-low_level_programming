ad8e70941e9c3039a130fe78cd38d594e3b0036d
Okolie James <zebedee788@gmail.com>
2022-09-27 03:46:32 -0700

    locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,24 +25,3 @@ char *_strstr(char *haystack, char *needle)
 	return (0);
 }
 
-/**
- *_strlen - returns the length of a string.
- *
- *@s: pointer to char
- *
- *Return: 0 (Success)
- */
-
-int _strlen(char *s)
-{
-	char *_str = s;
-	int cnt = 0;
-
-	while (*_str != '\0')
-	{
-		cnt++;
-		_str++;
-	}
-
-	return (cnt);
-}
