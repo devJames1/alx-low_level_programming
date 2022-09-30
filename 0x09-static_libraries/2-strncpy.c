00b9a4ddd4d730cc42688ad05e2ee5f39237c932
Okolie James <zebedee788@gmail.com>
2022-09-30 13:29:23 -0700

    Create the static library libmy.a

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function copies a string
+ *
+ *@src: char pointer
+ *@dest: char pointer
+ *@n: int for bytes
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+		int i;
+
+		for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[i] = src[i];
+		for ( ; i < n; i++)
+			dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ *_strlen - returns the length of a string.
+ *
+ *@s: pointer to char
+ *
+ *Return: 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	char *_str = s;
+	int cnt = 0;
+
+	while (*_str != '\0')
+	{
+		cnt++;
+		_str++;
+	}
+
+	return (cnt);
+}
